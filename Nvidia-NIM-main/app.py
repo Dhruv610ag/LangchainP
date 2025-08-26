1ad6ebb76cf2222f985f67b599d3ae56fc2ab359
Dhruv610ag <dhruv610agg@gmail.com>
2025-08-26 13:16:55 +0530

    Nvidia Nim

---
+++ Nvidia-NIM-main/app.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "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"
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama-3.1-70b-instruct",
+  messages=[{"role":"user","content":"Write an essay in about 200 words on Machine Learning"}],
+  temperature=0.2,
+  top_p=0.7,
+  max_tokens=1024,
+  stream=True
+)
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
